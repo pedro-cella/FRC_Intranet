bb13ec50e238c2477f9522f9b563a17b5d8cf4b2
Leonardo Gomes <leonardodasigomes@gmail.com>
2023-07-16 18:49:48 -0300

    Add: lab about www
    Co-authored-by: francisco1code <francisco1code@gmail.com>
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>
    Co-authored-by: Paulo Gonçalves Lima <paulogoncalves436@gmail.com>

--- DNS/config.sh
+++ DNS/config.sh
@@ -1,1 +1,5 @@
-cp ./resolv.conf > /etc/resolv.conf
+cat ./resolv >> /etc/resolv.conf
+cat ./hosts >> /etc/hosts
+cat ./named.conf.local > /etc/bind/named.conf.local
+cat ./db.queropassar > /etc/bind/db.queropassar
+cat ./db.queroreprovar > /etc/bind/db.queroreprovar
