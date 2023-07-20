2ac899309256ef5f3944b9882dd5159f44add82c
Leonardo Gomes <leonardodasigomes@gmail.com>
2023-07-20 10:10:47 -0300

    Adjust: web container name
    Co-authored-by: francisco1code <francisco1code@gmail.com>
    Co-authored-by: Pedro Cella <pedrovscella@gmail.com>
    Co-authored-by: Paulo Gonçalves Lima <paulogoncalves436@gmail.com>

--- DNS/config.sh
+++ DNS/config.sh
@@ -1,8 +1,8 @@
-cat ./resolv.conf >> /etc/resolv.conf
+cat config/resolv.conf >> /etc/resolv.conf
 
-cat ./hosts >> /etc/hosts
+cat config/hosts >> /etc/hosts
 
-cat ./named.conf.local > /etc/bind/named.conf.local
+cat config/named.conf.local > /etc/bind/named.conf.local
 
-cat ./db.flpp > /etc/bind/db.flpp
-cat ./db.pplf > /etc/bind/db.pplf
+cat config/db.flpp > /etc/bind/db.flpp
+cat config/db.pplf > /etc/bind/db.pplf
--- WEB/config/config.sh
+++ WEB/config/config.sh
@@ -1,4 +1,6 @@
 service network-manager start
+
 cat index.html > /var/www/html/index.html
-cat hosts > /etc/hosts
+cat hosts >> /etc/hosts
+
 service network-manager restart
